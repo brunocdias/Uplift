155721b998cbe1bee456c016a7dd90a9f6010262
Bruno Cesar <bcdbruno@gmail.com>
2020-12-30 17:14:01 -0300

    Include Service CRUD and HomePage

--- Uplift.DatAccess/Data/Repository/IRepository/IUnitOfWork.cs
+++ Uplift.DatAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -8,6 +8,7 @@ namespace Uplift.DatAccess.Data.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         IFrequencyRepository Frequency { get; }
+        IServiceRepository Service { get; }
 
         void Save();
     }
--- Uplift/wwwroot/js/service.js
+++ Uplift/wwwroot/js/service.js
@@ -7,23 +7,25 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url": "/admin/frequency/GetAll",
+            "url": "/admin/service/GetAll",
             "type": "GET",
             "datatype": "json"
         },
         "columns": [
-            { "data": "name", "width": "25%" },
-            { "data": "frequencyCount", "width": "25%" },
+            { "data": "name", "width": "20%" },
+            { "data": "category.name", "width": "20%" },
+            { "data": "price", "width": "15%" },
+            { "data": "frequency.frequencyCount", "width": "15%" },
             {
                 "data": "id",
                 "render": function (data) {
 
                     return `<div class="text-center"> 
-                            <a href="/Admin/frequency/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;' >
+                            <a href="/Admin/service/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;' >
                                 <i class='far fa-edit'></i> Edit
                             </a>
                             &nbsp;
-                            <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/admin/frequency/Delete/'+${data})>
+                            <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/admin/service/Delete/'+${data})>
                                <i class='far fa-trash-alt'></i> Delete
                             </a></div>
                         `;
